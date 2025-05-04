348fcf06e483c7d8d862f8c53e8cb2171899f080
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-05-04 15:55:07 +0530

    created register function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,57 @@
-export const signup = (req, res) => {};
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body;
+  try {
+    //All the details are present or not
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "Details are missing" });
+    }
+
+    // Password length greater than 6 or not
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must have at least 6 characters" });
+    }
+
+    // User exists or not
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //password to hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //Create new user
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // Generate jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      return res.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in Signup", error.message);
+    res.status(500).json({ message: "Internal Server error" });
+  }
+};
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
