774291edf68378b8de8452cd5c6d04ed4af279f6
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-05-04 21:32:51 +0530

    added auth backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,58 +1,56 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { email, fullName, password } = req.body;
   try {
-    //All the details are present or not
+    // Check if all details are provided
     if (!email || !fullName || !password) {
       return res.status(400).json({ message: "Details are missing" });
     }
 
-    // Password length greater than 6 or not
+    // Password length should be greater than or equal to 6
     if (password.length < 6) {
       return res
         .status(400)
         .json({ message: "Password must have at least 6 characters" });
     }
 
-    // User exists or not
+    // Check if user already exists
     const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    //password to hashed password
+    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    //Create new user
+    // Create new user
     const newUser = new User({
       fullName,
       email,
       password: hashedPassword,
     });
 
-    if (newUser) {
-      // Generate jwt token
-      generateToken(newUser._id, res);
-      await newUser.save();
+    // Save new user and generate JWT token
+    await newUser.save();
+    generateToken(newUser._id, res);
 
-      return res.status(200).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        email: newUser.email,
-        profilePic: newUser.profilePic,
-      });
-    } else {
-      return res.status(400).json({ message: "Invalid user data" });
-    }
+    return res.status(200).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      profilePic: newUser.profilePic,
+    });
   } catch (error) {
     console.log("Error in Signup", error.message);
-    res.status(500).json({ message: "Internal Server error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -81,12 +79,52 @@ export const login = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout", error.message);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile Pic does not exist" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Profile Pic Updated",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log("Error in updateProfile", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const checkAuth = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in check auth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import AuthRoutes from "./routes/auth.route.js";
 import { connectDb } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", AuthRoutes);
 
