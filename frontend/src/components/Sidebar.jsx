f5bbc4e687d01b1f81bf74e56a6949d61fed4773
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-06-28 20:05:32 +0530

    Temp commit Need to fix backend

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import SidebarSkeleton from "./SidebarSkeleton";
+import { Users } from "lucide-react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { getUsers, users, selectedUser, setSelectedUser, isUserLoading } =
+    useChatStore();
+
+  const onlineUsers = [];
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  if (isUserLoading) return <SidebarSkeleton />;
+
+  return (
+    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+      <div className="border-b border-base-300 w-full p-5">
+        <div className="flex items-center gap-2">
+          <Users className="size-6" />
+          <span className="font-medium hidden lg:block">Contacts</span>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
