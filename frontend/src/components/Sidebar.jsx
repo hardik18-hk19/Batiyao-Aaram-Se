02ee73a9f0eadfd467cb5958934e5f548f8dbbf3
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-06-29 01:16:16 +0530

    Added Message input and Chat Header and skeleton

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,12 +3,13 @@ import { useChatStore } from "../store/useChatStore";
 
 import SidebarSkeleton from "./SidebarSkeleton";
 import { Users } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } =
     useChatStore();
 
-  const onlineUsers = [];
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getUsers();
