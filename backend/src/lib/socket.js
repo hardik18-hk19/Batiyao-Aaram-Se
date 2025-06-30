7f88e29181f640f6430e85581b3f3c0614120946
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-01 00:46:57 +0530

    fix : cors deployment error fix

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,15 +8,16 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
+    credentials: true,
   },
 });
+
+//To store online users
+const userSocketMap = {};
 
 export function getRecieverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-//To store online users
-const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
