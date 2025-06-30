244bac909e6020f0d23fcdbdc2cc45b037ab01b1
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-01 00:33:51 +0530

    made deployment ready

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8580/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:8580/api"
+      : "/api",
   withCredentials: true,
 });
